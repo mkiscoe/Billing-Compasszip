5c3464b491d056c62e4d4d45f9d5d2e219b54ff3
mkiscoe <michelle.kiscoe@gmail.com>
2026-06-02 05:26:04 -0400

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-    port: 5000,
-    allowedHosts: true,
+    port: 8080,
     hmr: {
       overlay: false,
     },
